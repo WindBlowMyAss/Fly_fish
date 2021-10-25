12b631f6a82784ee365a94b6245a5a29c5abd91d
yaofeiyu <1048881641@qq.com>
2021-10-25 23:41:38 +0800

    upload codes

---
+++ app/src/main/java/com/example/fdutext/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fdutext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnTextView = findViewById(R.id.btn_textview);
+        mBtnTextView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,TextViewActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/fdutext/TextViewActivity2.java
@@ -0,0 +1,14 @@
+package com.example.fdutext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextViewActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view2);
+    }
+}
