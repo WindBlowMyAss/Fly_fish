bea34d63d8e0a01f8a179feb6a04e090b9cf28ea
yaofeiyu <1048881641@qq.com>
2021-11-09 09:32:04 +0800

    upload new file

---
+++ FDUTEXT3/app/src/main/java/com/example/fdutext3/ItemAdapter.java
@@ -0,0 +1,71 @@
+package com.example.fdutext3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
+    private Context context;
+    private List<ItemModel> items;
+    private ItemController itemController;
+
+    public ItemAdapter(Context context, List<ItemModel> items) {
+        this.context = context;
+        this.items = items;
+        this.itemController = new ItemController(context);
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ItemHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+
+        // 监听cbox
+        CheckBox cbox = holder.itemView.findViewById(R.id.cbox);
+        cbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int pos = holder.getAdapterPosition();
+                items.get(pos).setChecked(isChecked);
+                itemController.updateItem(items.get(pos)); // 更新数据库
+                holder.updateDisplay();
+            }
+        });
+
+        // 监听img_cross
+        holder.itemView.findViewById(R.id.img_cross).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = holder.getAdapterPosition();
+                itemController.deleteItem(items.get(pos)); // 更新数据库
+                items.remove(pos);
+                updateItems(items);
+            }
+        });
+
+        holder.bind(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void updateItems(List<ItemModel> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+}
---
+++ FDUTEXT3/app/src/main/java/com/example/fdutext3/ItemHolder.java
@@ -0,0 +1,40 @@
+package com.example.fdutext3;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemHolder extends RecyclerView.ViewHolder {
+    private CheckBox cbox;
+    private TextView tv_content, tv_date;
+
+    public ItemHolder(@NonNull View itemView) {
+        super(itemView);
+        cbox = itemView.findViewById(R.id.cbox);
+        tv_content = itemView.findViewById(R.id.tv_content);
+        tv_date = itemView.findViewById(R.id.tv_date);
+    }
+
+    public void bind(ItemModel model) {
+        tv_content.setText(model.getContent());
+        tv_date.setText(model.getDate());
+        cbox.setChecked(model.isChecked());
+        updateDisplay();
+    }
+
+    public void updateDisplay() {
+        if (cbox.isChecked()) {
+            tv_content.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            tv_content.setTextColor(Color.GRAY);
+        }
+        else {
+            tv_content.getPaint().setFlags(0);
+            tv_content.setTextColor(Color.BLACK);
+        }
+    }
+}
